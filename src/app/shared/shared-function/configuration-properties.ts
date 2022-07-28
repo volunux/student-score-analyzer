663e9408e334f1d7efd3b076a391a4c48d3eae3d
Yusuf Musa <volunux@gmail.com>
2022-07-28 22:12:14 +0100

    Configs setting

--- src/app/shared/shared-function/configuration-properties.ts
+++ src/app/shared/shared-function/configuration-properties.ts
@@ -1,6 +1,6 @@
 export class ConfigurationProperties {
 
   public static getApiHost(): string {
-    return "https://desolate-harbor-08314.herokuapp.com/";
+    return "https://desolate-harbor-08314.herokuapp.com";
   }
 }
